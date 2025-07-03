766a3e87048099df9695d90394982764f9ceaaae
FRedhunter <busrabetulbayram@hotmail.com>
2025-07-04 00:06:44 +0300

    Title has changed

--- src/app/components/Discussions.jsx
+++ src/app/components/Discussions.jsx
@@ -7,11 +7,11 @@ export default function Discussions() {
   const [discussions] = useState(discussionsData)
 
   return (
-    <ul className='discussions divide-y divide-gray-200'>
-      {discussions.map((item) => (
+    <ul className='divide-y divide-gray-200'>
+      {discussions.map((item, index) => (
         <Discussion
           key={item.id}
-          title={item.title}
+          title={`Tartışma konusu #${index + 1}`}
           author={item.author}
           createdAt={item.createdAt}
         />
